c66181059a668749aece769263a7850769394d95
David Ko <david@barobo.com>
2016-10-03 13:56:07 -0700

    Added cpp to code generation

--- build.py
+++ build.py
@@ -181,6 +181,7 @@ class Gen_compressed(threading.Thread):
     self.gen_generator("dart")
     self.gen_generator("lua")
     self.gen_generator("ch")
+    self.gen_generator("cpp")
 
   def gen_core(self):
     target_filename = "blockly_compressed.js"
